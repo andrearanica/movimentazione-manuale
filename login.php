613a2337560d445dcbf062051ad1c12c6922331a
Andrea Ranica <andrearanica2004@gmail.com>
2023-02-14 14:27:15 +0100

    ✨ Login and redirect

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+
+ini_set('display_errors', 1);
+
+$ip = '127.0.0.1';
+$user = 'root';
+$password = '';
+$db = 'my_andrearanica';
+
+$connection = new mysqli($ip, $user, $password, $db);
+
+if (isset($_POST['username']) && isset($_POST['username'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+}
+
+
+$query = 'SELECT * FROM users WHERE username="' . $username . '" AND password="' . $password .'";';
+$result = $connection->query($query);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $_SESSION['name_surname'] = $row['name_surname'];
+    $_SESSION['username'] = $row['username'];
+    $_SESSION['password'] = $row['password'];
+
+    if ($row['role'] == 0) {
+        header('Location: php');
+    } else if ($row['role'] == 1) {
+        header('Location: admin.php');
+    }
+}
+
+?>
